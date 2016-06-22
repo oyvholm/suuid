afb26dfa8cc78e76a1e817b4f335317bd6dc4ee7
Øyvind A. Holm <sunny@sunbase.org>
2016-06-22 21:03:30 +0200

    Move XML functions from suuid.c to logfile.c, 67/238 fail
    Move init_xml_entry(), allocate_entry(), alloc_attr() and xml_entry() to
    logfile.c, the format they're dealing with is only used in the log file.
    
    7fed5304-38ac-11e6-98e5-02010e0a6634

--- src/suuid.h
+++ src/suuid.h
@@ -104,6 +104,10 @@ extern char *get_username(void);
 extern char *get_tty(void);
 
 /* logfile.c */
+extern void init_xml_entry(struct Entry *);
+extern char *allocate_entry(char *, char *);
+extern char *alloc_attr(char *, char *);
+extern char *xml_entry(struct Entry *);
 extern char *get_logdir();
 extern int add_to_logfile(char *, struct Entry *);
 extern void create_logfile(char *);
@@ -116,10 +120,6 @@ extern void print_version(void);
 extern void usage(int);
 extern int choose_opt_action(struct Options *, int, struct option *);
 extern int parse_options(struct Options *, int, char *[]);
-extern void init_xml_entry(struct Entry *);
-extern char *allocate_entry(char *, char *);
-extern char *alloc_attr(char *, char *);
-extern char *xml_entry(struct Entry *);
 
 /* utf8.c */
 extern unsigned char *utf8_check(unsigned char *);
