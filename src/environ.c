fe77e3fc14c36ab6d2e2ea7815a965d2cf1ce215
Øyvind A. Holm <sunny@sunbase.org>
2016-06-27 20:35:33 +0200

    Add fixme to comment
    f655bab2-3c95-11e6-ab32-13495fa3cc2c

--- src/environ.c
+++ src/environ.c
@@ -109,8 +109,8 @@ char *get_tty(void)
 
 	retval = ttyname(STDIN_FILENO);
 
-	/* Legacy reasons, the Perl version of suuid calls `tty` from GNU 
-	 * coreutils, and it prints "not a tty\n" when ttyname() fails. The 
+	/* fixme: Legacy reasons, the Perl version of suuid calls `tty` from 
+	 * GNU coreutils, and it prints "not a tty\n" when ttyname() fails. The 
 	 * right thing to do is to return NULL so the tty element is omitted 
 	 * from the logs.
 	 */
