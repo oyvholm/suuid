21efe7e49060f535e42c6b3394c238408d8144a9
Øyvind A. Holm <sunny@sunbase.org>
2016-07-09 08:38:32 +0200

    Add templates for sess.c and sess.h
    cf963a42-459f-11e6-a2b2-0bae6f17a71b

---
+++ src/sess.c
@@ -0,0 +1,268 @@
+/*
+ * sess.c
+ * File ID: 3e521c4a-459f-11e6-9d59-6f894a58dace
+ *
+ * (C)opyleft 2016- Øyvind A. Holm <sunny@sunbase.org>
+ *
+ * This program is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by the Free 
+ * Software Foundation; either version 2 of the License, or (at your option) 
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for 
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "sess.h"
+
+/*
+ * Global variables
+ */
+
+char *progname;
+struct Options opt;
+
+/*
+ * msg() - Print a message prefixed with "[progname]: " to stddebug if 
+ * opt.verbose is equal or higher than the first argument. The rest of the 
+ * arguments are delivered to vfprintf().
+ * Returns the number of characters written.
+ */
+
+int msg(int verbose, const char *format, ...)
+{
+	va_list ap;
+	int retval = 0;
+
+	if (opt.verbose >= verbose) {
+		va_start(ap, format);
+		retval = fprintf(stddebug, "%s: ", progname);
+		retval += vfprintf(stddebug, format, ap);
+		retval += fprintf(stddebug, "\n");
+		va_end(ap);
+	}
+
+	return retval;
+}
+
+/*
+ * myerror() - Print an error message to stderr using this format:
+ *   a: b: c
+ * where a is the name of the program (progname), b is the output from the 
+ * printf-like string and optional arguments, and c is the error message from 
+ * errno. Returns the number of characters written.
+ */
+
+int myerror(const char *format, ...)
+{
+	va_list ap;
+	int retval = 0;
+	int orig_errno = errno;
+
+	retval = fprintf(stderr, "%s: ", progname);
+	va_start(ap, format);
+	retval += vfprintf(stderr, format, ap);
+	va_end(ap);
+	if (orig_errno)
+		retval += fprintf(stderr, ": %s", strerror(orig_errno));
+	retval += fprintf(stderr, "\n");
+
+	return retval;
+}
+
+/*
+ * print_license() - Display the program license
+ */
+
+void print_license(void)
+{
+	puts("(C)opyleft 2016- Øyvind A. Holm <sunny@sunbase.org>");
+	puts("");
+	puts("This program is free software; you can redistribute it "
+	     "and/or modify it \n"
+	     "under the terms of the GNU General Public License as "
+	     "published by the \n"
+	     "Free Software Foundation; either version 2 of the License, "
+	     "or (at your \n"
+	     "option) any later version.");
+	puts("");
+	puts("This program is distributed in the hope that it will be "
+	     "useful, but \n"
+	     "WITHOUT ANY WARRANTY; without even the implied warranty of \n"
+	     "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.");
+	puts("See the GNU General Public License for more details.");
+	puts("");
+	puts("You should have received a copy of "
+	     "the GNU General Public License along \n"
+	     "with this program. If not, see "
+	     "<http://www.gnu.org/licenses/>.");
+}
+
+/*
+ * print_version() - Print version information on stdout
+ */
+
+void print_version(void)
+{
+	printf("%s %s (%s)\n", progname, VERSION, RELEASE_DATE);
+}
+
+/*
+ * usage() - Prints a help screen
+ */
+
+void usage(int retval)
+{
+	if (retval != EXIT_OK) {
+		fprintf(stderr, "\nType \"%s --help\" for help screen. "
+		                "Returning with value %d.\n",
+		                progname, retval);
+		return;
+	}
+	puts("");
+	if (opt.verbose >= 1) {
+		print_version();
+		puts("");
+	}
+	printf("Usage: %s [options]\n", progname);
+	printf("\n");
+	printf("Options:\n");
+	printf("\n");
+	printf("  -h, --help\n"
+	       "    Show this help.\n");
+	printf("  --license\n"
+	       "    Print the software license\n");
+	printf("  -q, --quiet\n"
+	       "    Be more quiet. Can be repeated to increase silence.\n");
+	printf("  -v, --verbose\n"
+	       "    Increase level of verbosity. Can be repeated.\n");
+	printf("  --version\n"
+	       "    Print version information.\n");
+	printf("\n");
+}
+
+/*
+ * choose_opt_action() - Decide what to do when option c is found. Store 
+ * changes in dest. opts is the struct with the definitions for the long 
+ * options.
+ * Return EXIT_OK if ok, EXIT_ERROR if c is unknown or anything fails.
+ */
+
+int choose_opt_action(struct Options *dest, int c, struct option *opts)
+{
+	int retval = EXIT_OK;
+
+	switch (c) {
+	case 0:
+		if (!strcmp(opts->name, "license"))
+			dest->license = TRUE;
+		else if (!strcmp(opts->name, "version"))
+			dest->version = TRUE;
+		break;
+	case 'h':
+		dest->help = TRUE;
+		break;
+	case 'q':
+		dest->verbose--;
+		break;
+	case 'v':
+		dest->verbose++;
+		break;
+	default:
+		msg(3, "getopt_long() returned character code %d", c);
+		retval = EXIT_ERROR;
+		break;
+	}
+
+	return retval;
+}
+
+/*
+ * parse_options() - Parse command line options.
+ * Returns EXIT_OK if ok, EXIT_ERROR if error.
+ */
+
+int parse_options(struct Options *dest, int argc, char *argv[])
+{
+	int retval = EXIT_OK;
+	int c;
+
+	dest->help = FALSE;
+	dest->license = FALSE;
+	dest->verbose = 0;
+	dest->version = FALSE;
+
+	while (retval == EXIT_OK) {
+		int option_index = 0;
+		static struct option long_options[] = {
+			{"help", no_argument, 0, 'h'},
+			{"license", no_argument, 0, 0},
+			{"quiet", no_argument, 0, 'q'},
+			{"verbose", no_argument, 0, 'v'},
+			{"version", no_argument, 0, 0},
+			{0, 0, 0, 0}
+		};
+
+		c = getopt_long(argc, argv,
+		                "h"  /* --help */
+		                "q"  /* --quiet */
+		                "v"  /* --verbose */
+		                , long_options, &option_index);
+
+		if (c == -1)
+			break;
+
+		retval = choose_opt_action(dest,
+		                           c, &long_options[option_index]);
+	}
+
+	return retval;
+}
+
+/*
+ * main()
+ */
+
+int main(int argc, char *argv[])
+{
+	int retval = EXIT_OK;
+
+	progname = argv[0];
+
+	retval = parse_options(&opt, argc, argv);
+	if (retval != EXIT_OK) {
+		fprintf(stderr, "%s: Option error\n", progname);
+		return EXIT_ERROR;
+	}
+
+	msg(3, "Using verbose level %d", opt.verbose);
+
+	if (opt.help) {
+		usage(EXIT_OK);
+		return EXIT_OK;
+	} else if (opt.version) {
+		print_version();
+		return EXIT_OK;
+	} else if (opt.license) {
+		print_license();
+		return EXIT_OK;
+	}
+
+	if (optind < argc) {
+		int t;
+
+		for (t = optind; t < argc; t++)
+			msg(3, "Non-option arg: %s", argv[t]);
+	}
+
+	msg(3, "Returning from main() with value %d", retval);
+
+	return retval;
+}
+
+/* vim: set ts=8 sw=8 sts=8 noet fo+=w tw=79 fenc=UTF-8 : */
