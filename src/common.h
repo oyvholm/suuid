6e6d49de26cfa9af220d272eba529c51af3039ac
Øyvind A. Holm <sunny@sunbase.org>
2016-07-11 02:45:57 +0200

    Create initial common.h and add it to Makefile
    de6f6a90-4700-11e6-81f6-1f61cb3575bc

---
+++ src/common.h
@@ -0,0 +1,26 @@
+/*
+ * common.h
+ * File ID: 0ee270ea-46f8-11e6-bad2-2ba27a9a54c1
+ *
+ * (C)opyleft 2016- Øyvind A. Holm <sunny@sunbase.org>
+ *
+ * This program is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by the Free 
+ * Software Foundation; either version 2 of the License, or (at your option) 
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for 
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _COMMON_H
+#define _COMMON_H
+
+#endif /* ifndef _COMMON_H */
+
+/* vim: set ts=8 sw=8 sts=8 noet fo+=w tw=79 fenc=UTF-8 : */
