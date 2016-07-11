9c056eba748fe4dac77f19a7c59939796495eaa8
Øyvind A. Holm <sunny@sunbase.org>
2016-07-11 02:58:40 +0200

    Move function prototypes from suuid.h to common.h
    a5dd8084-4702-11e6-80b9-2304d9db9414

--- src/common.h
+++ src/common.h
@@ -121,6 +121,92 @@ struct uuid_result {
 	bool success;
 };
 
+/*
+ * Function prototypes
+ */
+
+#if 1 /* Set to 0 to test without prototypes */
+
+/* environ.c */
+extern char *get_editor(void);
+extern bool valid_hostname(const char *s);
+extern char *get_hostname(const struct Rc *rc);
+extern char *getpath(void);
+extern char *get_username(void);
+extern char *get_tty(void);
+
+/* genuuid.c */
+extern bool init_randomness(void);
+extern char *process_comment_option(const char *cmt);
+extern int fill_entry_struct(struct Entry *entry, const struct Rc *rc,
+                             const struct Options *opt);
+extern char *process_uuid(FILE *logfp, const struct Rc *rc,
+                          const struct Options *opt, struct Entry *entry);
+extern void sighandler(const int sig);
+extern struct uuid_result create_and_log_uuids(const struct Options *opt);
+
+/* io.h */
+extern char *read_from_fp(FILE *fp);
+extern char *read_from_file(const char *fname);
+extern char *read_from_editor(const char *editor);
+
+/* logfile.c */
+extern bool valid_xml_chars(const char *s);
+extern char *suuid_xml(const char *text);
+extern void init_xml_entry(struct Entry *e);
+extern char *allocate_elem(const char *elem, const char *src);
+extern char *alloc_attr(const char *attr, const char *data);
+extern char *get_xml_tags(const struct Entry *entry);
+extern char *create_sess_xml(const struct Entry *entry);
+extern char *xml_entry(const struct Entry *entry, const bool raw);
+extern char *get_logdir(const struct Options *opt);
+extern char *get_logfile_name(const struct Rc *rc, const struct Options *opt);
+extern FILE *lock_file(FILE *fp, const char *fname);
+extern FILE *write_xml_header(FILE *fp);
+extern FILE *seek_to_eof(FILE *fp, const char *fname);
+extern FILE *unknown_end_line(FILE *fp, const char *fname);
+extern FILE *check_last_log_line(FILE *fp, const char *fname);
+extern FILE *seek_to_entry_pos(FILE *fp, const char *fname);
+extern FILE *open_logfile(const char *fname);
+extern int add_to_logfile(FILE *fp, const struct Entry *entry, const bool raw);
+extern int close_logfile(FILE *fp);
+
+/* rcfile.c */
+extern char *get_rcfilename(const struct Options *opt);
+extern char *has_key(const char *line, const char *keyword);
+extern int parse_rc_line(const char *line, struct Rc *rc);
+extern int read_rcfile(const char *rcfile, struct Rc *rc);
+
+/* sessvar.c */
+extern bool is_legal_desc_char(const unsigned char c);
+extern bool is_valid_desc_string(const char *s);
+extern int fill_sess(struct Entry *dest, const char *uuid,
+                     const char *desc, const size_t desclen);
+extern int get_sess_info(struct Entry *entry);
+
+/* string.c */
+extern char *check_hex(const char *hex, const size_t len);
+extern char *trim_str_front(char *dest);
+extern char *trim_str_end(char *dest);
+extern char *utf8_check(const char *text);
+
+/* tag.c */
+extern void rewind_tag(void);
+extern bool tag_exists(const struct Entry *entry, const char *tag);
+extern char *get_next_tag(const struct Entry *entry);
+extern char *store_tag(struct Entry *entry, const char *arg);
+
+/* uuid.c */
+extern bool valid_uuid(const char *u, const bool check_len);
+extern char *scramble_mac_address(char *uuid);
+extern char *generate_uuid(const struct Rc *rc, const bool random_mac);
+extern char *uuid_date(char *dest, const char *uuid);
+extern bool is_valid_date(const char *s, const bool check_len);
+extern char *uuid_date_from_uuid(char *dest, const char *uuid);
+extern char *scan_for_uuid(const char *s);
+
+#endif
+
 #endif /* ifndef _COMMON_H */
 
 /* vim: set ts=8 sw=8 sts=8 noet fo+=w tw=79 fenc=UTF-8 : */
