2a55c96d752109fe94818877f2c2623898e7bd24
Øyvind A. Holm <sunny@sunbase.org>
2016-07-13 01:22:36 +0200

    suuid 0.2.2
    No new functionality or bugfixes, only code churn and changes necessary
    for sess(1) which is developed on another branch.
    
    d41561de-4887-11e6-b1a9-93443407614c

--- src/suuid.h
+++ src/suuid.h
@@ -25,8 +25,8 @@
  * Defines
  */
 
-#define VERSION       "0.2.1"
-#define RELEASE_DATE  "2016-07-10"
+#define VERSION       "0.2.2"
+#define RELEASE_DATE  "2016-07-12"
 
 #define FALSE  0
 #define TRUE   1
