5b7e2ba6cb2e0caccec69b6d0e9afe1c2cbd34cf
Øyvind A. Holm <sunny@sunbase.org>
2016-10-26 05:44:29 +0200

    synced.sql: Mark src/Makefile as updated
    7fdd7792-9b2e-11e6-a855-7a7859f468c3

--- synced.sql
+++ synced.sql
@@ -22,7 +22,7 @@ CREATE TABLE synced (
 INSERT INTO "synced" VALUES('conv-suuid','Lib/std/perl','5d9a5b337cbc4997dfa181e3fd9a9484487b5605','2016-10-25 02:43:03');
 INSERT INTO "synced" VALUES('obsolete/suuid.pl','','273d6f5b1b1e7eb6c8553cd2cb3b176c5678ac7c','2016-05-17 14:10:28');
 INSERT INTO "synced" VALUES('src/COPYING','Lib/std/COPYING','17bc73d79f068dfaf2655db1090f6b9206734555','2016-06-06 03:02:53');
-INSERT INTO "synced" VALUES('src/Makefile','Lib/std/c/Makefile','c707e16461a3b371a6e270a285d9ac200b8a1e76','2016-07-07 13:15:36');
+INSERT INTO "synced" VALUES('src/Makefile','Lib/std/c/Makefile','5d9a5b337cbc4997dfa181e3fd9a9484487b5605','2016-10-25 02:43:03');
 INSERT INTO "synced" VALUES('src/common.h','Lib/std/c/std.h','a4c18cda5d2ee2640fcfa27631312c6c651540e5','2016-07-12 01:57:35');
 INSERT INTO "synced" VALUES('src/sess.c','Lib/std/c/std.c','a4c18cda5d2ee2640fcfa27631312c6c651540e5','2016-07-12 01:57:35');
 INSERT INTO "synced" VALUES('src/sess.h','Lib/std/c/std.h','a4c18cda5d2ee2640fcfa27631312c6c651540e5','2016-07-12 01:57:35');
