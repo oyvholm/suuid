af580f243bb1e31b6bfdfc8ff9b74a21a6b770fb
Øyvind A. Holm <sunny@sunbase.org>
2016-11-20 22:34:55 +0100

    wi: Use /bin/sh instead of bash and add it to synced.sql, syncs nowhere
    It has its own special option parsing, so drop the sync for this one.
    
    5bae34d4-af69-11e6-b9a9-279c2a0468a3

--- synced.sql
+++ synced.sql
@@ -38,6 +38,7 @@ INSERT INTO "synced" VALUES('tests/finduuid.t','Lib/std/perl-tests-tab','c58b81b
 INSERT INTO "synced" VALUES('tests/sess.t','Lib/std/perl-tests','5d9a5b337cbc4997dfa181e3fd9a9484487b5605','2016-10-25 02:43:03');
 INSERT INTO "synced" VALUES('ti','Lib/std/sh','53d7643de23eed270013018ef12e7e80b2eb3f63','2016-11-20 16:04:29');
 INSERT INTO "synced" VALUES('tjah','Lib/std/sj','53d7643de23eed270013018ef12e7e80b2eb3f63','2016-11-20 16:04:29');
+INSERT INTO "synced" VALUES('wi','','',NULL);
 CREATE TABLE todo (
   file TEXT
     CONSTRAINT todo_file_length
