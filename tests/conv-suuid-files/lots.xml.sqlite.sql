c7702378ba26c51d1aba8749b03128c3dc703a64
Øyvind A. Holm <sunny@sunbase.org>
2016-11-30 10:47:45 +0100

    conv-suuid: Split all \n inside <txt></txt> into newlines
    \n characters were stored in the XML files as escaped '\n' to avoid
    linebreaks. Only doing it with --output-format sqlite for now, haven't
    tested it with Postgres yet.
    
    5e17f98c-b6e2-11e6-ab87-279c2a0468a3

--- tests/conv-suuid-files/lots.xml.sqlite.sql
+++ tests/conv-suuid-files/lots.xml.sqlite.sql
@@ -1,3 +1,8 @@
 BEGIN TRANSACTION;
-INSERT INTO uuids (t,u,tag,host,cwd,username,tty,sess,txt,s) VALUES('2016-11-30T08:49:39.2512000Z','eda46800-b6d9-11e6-96f0-279c2a0468a3',NULL,'','','','','[""]','Lots of stuff!\nNew line.\n&lt; &amp; &gt;\n\\\n''\n"','<suuid t="2016-11-30T08:49:39.2512000Z" u="eda46800-b6d9-11e6-96f0-279c2a0468a3"> <txt>Lots of stuff!\nNew line.\n&lt; &amp; &gt;\n\\\n''\n"</txt> </suuid>');
+INSERT INTO uuids (t,u,tag,host,cwd,username,tty,sess,txt,s) VALUES('2016-11-30T08:49:39.2512000Z','eda46800-b6d9-11e6-96f0-279c2a0468a3',NULL,'','','','','[""]','Lots of stuff!
+New line.
+&lt; &amp; &gt;
+\\
+''
+"','<suuid t="2016-11-30T08:49:39.2512000Z" u="eda46800-b6d9-11e6-96f0-279c2a0468a3"> <txt>Lots of stuff!\nNew line.\n&lt; &amp; &gt;\n\\\n''\n"</txt> </suuid>');
 COMMIT;
