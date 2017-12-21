84c06521198cca877ead30faf5e50db3f6c8f1e1
Øyvind A. Holm <sunny@sunbase.org>
2017-12-21 14:06:45 +0100

    synced.sql: Mark the rest of the files as synced
    d62fad8c-e64f-11e7-bff4-f74d993421b0

--- synced.sql
+++ synced.sql
@@ -25,11 +25,11 @@ INSERT INTO synced VALUES('needuuid','Lib/std/perl-tab','c6365e55b0510bc1a385dc4
 INSERT INTO synced VALUES('obsolete/suuid.pl','','273d6f5b1b1e7eb6c8553cd2cb3b176c5678ac7c','2016-05-17 14:10:28');
 INSERT INTO synced VALUES('src/COPYING','Lib/std/COPYING','17bc73d79f068dfaf2655db1090f6b9206734555','2016-06-06 03:02:53');
 INSERT INTO synced VALUES('src/Gen-version','Lib/std/c/Gen-version','2ef036df30fe3f312c770a85f0c4c4ee23e7bfbe','2017-12-20 18:50:23');
-INSERT INTO synced VALUES('src/Makefile','Lib/std/c/Makefile','4f1d59b1929757ad25b2321821fce598627d5d55','2017-10-02 15:10:57');
-INSERT INTO synced VALUES('src/common.h','Lib/std/c/std.h','4f1d59b1929757ad25b2321821fce598627d5d55','2017-10-02 15:10:57');
+INSERT INTO synced VALUES('src/Makefile','Lib/std/c/Makefile','6277f2b035d8cab6d50c0d102ae626e54bd3bb72','2017-12-21 12:56:56');
+INSERT INTO synced VALUES('src/common.h','Lib/std/c/std.h','6277f2b035d8cab6d50c0d102ae626e54bd3bb72','2017-12-21 12:56:56');
 INSERT INTO synced VALUES('src/gdbrc','Lib/std/c/gdbrc','4f1d59b1929757ad25b2321821fce598627d5d55','2017-10-02 15:10:57');
-INSERT INTO synced VALUES('src/selftest.c','Lib/std/c/selftest.c','9746b7b61124066a687a57641c12a3a1e363b0ee','2017-12-18 16:37:23');
-INSERT INTO synced VALUES('src/suuid.c','Lib/std/c/std.c','4f1d59b1929757ad25b2321821fce598627d5d55','2017-10-02 15:10:57');
+INSERT INTO synced VALUES('src/selftest.c','Lib/std/c/selftest.c','6277f2b035d8cab6d50c0d102ae626e54bd3bb72','2017-12-21 12:56:56');
+INSERT INTO synced VALUES('src/suuid.c','Lib/std/c/std.c','6277f2b035d8cab6d50c0d102ae626e54bd3bb72','2017-12-21 12:56:56');
 INSERT INTO synced VALUES('src/suuid.h','Lib/std/c/std.h','6277f2b035d8cab6d50c0d102ae626e54bd3bb72','2017-12-21 12:56:56');
 INSERT INTO synced VALUES('src/t/suuid.t','Lib/std/perl-tests','5d9a5b337cbc4997dfa181e3fd9a9484487b5605','2016-10-25 02:43:03');
 INSERT INTO synced VALUES('tests/Add_test','Lib/std/Add_test','78318122fa93a5f874778eaf106f43f4026131fe','2017-08-28 19:06:17');
