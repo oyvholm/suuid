6cee526a3947d18420e40fc2831c394db00719cc
Øyvind A. Holm <sunny@sunbase.org>
2017-12-21 13:49:00 +0100

    Gen-version: Don't need the #ifndef, only write #defines
    It's only included from std.h, and it already has a test like that. It
    also makes it easier to add defines to the file.
    
    87ce0b0e-e64d-11e7-84ae-f74d993421b0

--- synced.sql
+++ synced.sql
@@ -24,7 +24,7 @@ INSERT INTO synced VALUES('finduuid','Lib/std/perl-tab','c58b81b5652b062928cc7bd
 INSERT INTO synced VALUES('needuuid','Lib/std/perl-tab','c6365e55b0510bc1a385dc497ff4d958b003c012','2016-11-11 21:46:30');
 INSERT INTO synced VALUES('obsolete/suuid.pl','','273d6f5b1b1e7eb6c8553cd2cb3b176c5678ac7c','2016-05-17 14:10:28');
 INSERT INTO synced VALUES('src/COPYING','Lib/std/COPYING','17bc73d79f068dfaf2655db1090f6b9206734555','2016-06-06 03:02:53');
-INSERT INTO synced VALUES('src/Gen-version','Lib/std/c/Gen-version','9746b7b61124066a687a57641c12a3a1e363b0ee','2017-12-18 16:37:23');
+INSERT INTO synced VALUES('src/Gen-version','Lib/std/c/Gen-version','2ef036df30fe3f312c770a85f0c4c4ee23e7bfbe','2017-12-20 18:50:23');
 INSERT INTO synced VALUES('src/Makefile','Lib/std/c/Makefile','4f1d59b1929757ad25b2321821fce598627d5d55','2017-10-02 15:10:57');
 INSERT INTO synced VALUES('src/common.h','Lib/std/c/std.h','4f1d59b1929757ad25b2321821fce598627d5d55','2017-10-02 15:10:57');
 INSERT INTO synced VALUES('src/gdbrc','Lib/std/c/gdbrc','4f1d59b1929757ad25b2321821fce598627d5d55','2017-10-02 15:10:57');
