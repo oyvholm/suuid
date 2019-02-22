50c6e130741296f0c518aea24fd0140bd4ed1b88
Øyvind A. Holm <sunny@sunbase.org>
2019-02-22 02:31:55 +0100

    uuid.c: Copy puid() from utest.c, execute it in main()
    One small change in the printf() string (add "l"), otherwise unchanged.
    
    96effaca-3692-11e9-8830-4f45262dc9b5

--- doc/rfc-source/uuid.c
+++ doc/rfc-source/uuid.c
@@ -273,7 +273,26 @@ int uuid_compare(uuid_t *u1, uuid_t *u2)
 }
 #undef CHECK
 
+/* puid() is copied from utest.c */
+/* puid -- print a UUID */
+void puid(uuid_t u)
+{
+    int i;
+
+    printf("%8.8lx-%4.4x-%4.4x-%2.2x%2.2x-", u.time_low, u.time_mid,
+    u.time_hi_and_version, u.clock_seq_hi_and_reserved,
+    u.clock_seq_low);
+    for (i = 0; i < 6; i++)
+        printf("%2.2x", u.node[i]);
+    printf("\n");
+}
+
 int main(void)
 {
+	uuid_t uuid;
+
+	uuid_create(&uuid);
+	puid(uuid);
+
 	return EXIT_SUCCESS;
 }
