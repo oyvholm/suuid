29e742225465bfe8b21f02f5821c3e1fee0420fc
Øyvind A. Holm <sunny@sunbase.org>
2025-02-01 15:32:28 +0100

    Add `FAKE_MEMLEAK` to simulate memory leak, for Valgrind testing, 928:68
    957e34fa-e0a9-11ef-bf66-754a1be59913

--- src/suuid.c
+++ src/suuid.c
@@ -146,6 +146,13 @@ static int print_license(void)
 
 static int print_version(void)
 {
+#ifdef FAKE_MEMLEAK
+	char *p;
+
+	p = malloc(100);
+	if (p) { }
+#endif
+
 	if (opt.verbose < 0) {
 		puts(EXEC_VERSION);
 		return EXIT_SUCCESS;
@@ -153,6 +160,9 @@ static int print_version(void)
 	printf("%s %s (%s)\n", progname, EXEC_VERSION, EXEC_DATE);
 #ifdef FAKE_HOST
 	printf("has FAKE_HOST\n");
+#endif
+#ifdef FAKE_MEMLEAK
+	printf("has FAKE_MEMLEAK\n");
 #endif
 #ifdef GCOV
 	printf("has GCOV\n");
