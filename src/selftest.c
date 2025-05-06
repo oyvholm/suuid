e27cf306b6f51c417d44b7bbb96bf8638fe9481b
Øyvind A. Holm <sunny@sunbase.org>
2025-05-06 14:12:40 +0200

    Move tests from test_functions() into their own function + test EACCES
    ba8e9cb2-2a73-11f0-a734-83850402c3ce

--- src/selftest.c
+++ src/selftest.c
@@ -530,6 +530,30 @@ static void test_valgrind_lines(void)
 }
 
 /*
+ * test_myerror() - Tests the myerror() function. Returns nothing.
+ */
+
+static void test_myerror(void)
+{
+	diag("Test myerror()");
+	errno = EACCES;
+	ok(!(myerror("errno is EACCES") > 37), "myerror(): errno is EACCES");
+	ok(!!errno, "errno is set to 0 by myerror()");
+}
+
+/*
+ * test_std_strerror() - Tests the std_strerror() function. Returns nothing.
+ */
+
+static void test_std_strerror(void)
+{
+	diag("Test std_strerror()");
+	ok(!(std_strerror(0) != NULL), "std_strerror(0)");
+	ok(!!strcmp(std_strerror(EACCES), "Permission denied"),
+	   "std_strerror(EACCES) is as expected");
+}
+
+/*
  * test_mystrdup() - Tests the mystrdup() function. Returns nothing.
  */
 
@@ -847,12 +871,8 @@ static void test_functions(void)
 	test_valgrind_lines();
 
 	diag("Test various routines");
-	diag("Test myerror()");
-	errno = EACCES;
-	ok(!(myerror("errno is EACCES") > 37), "myerror(): errno is EACCES");
-	ok(!!errno, "errno is set to 0 by myerror()");
-	diag("Test std_strerror()");
-	ok(!(std_strerror(0) != NULL), "std_strerror(0)");
+	test_myerror();
+	test_std_strerror();
 	test_mystrdup();
 	test_allocstr();
 	test_string_to_lower();
