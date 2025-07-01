c73228bc25ec78b465af9f5523b4af76ed1cf3da
Øyvind A. Holm <sunny@sunbase.org>
2025-07-02 00:11:14 +0200

    selftest.c: Use surrounding whitespace in `chp{ }` macros where missing
    5d37bea0-56c8-11f0-90b7-83850402c3ce

--- src/selftest.c
+++ src/selftest.c
@@ -637,8 +637,8 @@ static void test_valgrind_option(char *execname, const struct Options *o)
 
 		mod_opt.valgrind = false; /* gncov */
 		streams_init(&ss); /* gncov */
-		streams_exec(&mod_opt, &ss, chp{"valgrind", /* gncov */
-		                                "--version", NULL});
+		streams_exec(&mod_opt, &ss, chp{ "valgrind", /* gncov */
+		                                 "--version", NULL });
 		if (!strstr(ss.out.buf, "valgrind-")) { /* gncov */
 			ok(1, "Valgrind is not installed," /* gncov */
 			      " disabling Valgrind checks");
@@ -649,7 +649,7 @@ static void test_valgrind_option(char *execname, const struct Options *o)
 		streams_free(&ss); /* gncov */
 	}
 
-	sc(chp{execname, "--valgrind", "-h", NULL},
+	sc(chp{ execname, "--valgrind", "-h", NULL },
 	   "Show this",
 	   "",
 	   EXIT_SUCCESS,
