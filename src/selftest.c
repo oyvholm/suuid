6a16400a6c4944d56ef6bdeab528690061fc29f4
Øyvind A. Holm <sunny@sunbase.org>
2025-08-06 02:44:33 +0200

    test_mystrdup(): Turn a failed() into failed_ok(), 914:163
    9d9b5720-725e-11f0-852f-83850402c3ce

--- src/selftest.c
+++ src/selftest.c
@@ -784,7 +784,7 @@ static void test_mystrdup(void)
 
 	s = mystrdup(txt);
 	if (!s) {
-		failed("mystrdup()"); /* gncov */
+		failed_ok("mystrdup()"); /* gncov */
 		return; /* gncov */
 	}
 	ok(!!strcmp(s, txt), "mystrdup(): Strings are identical");
